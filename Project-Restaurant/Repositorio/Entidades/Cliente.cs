d7697278264fb3a39f3192e2d3d5c8aa5312f56b
cristyan.alexandrino <cristyanalexandrino.od@gmail.com>
2022-08-29 21:33:38 -0300

    Criado Classe Cliente

---
+++ Project-Restaurant/Repositorio/Entidades/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio.Entidades
+{
+    internal class Cliente
+    { 
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+
+    }
+}
