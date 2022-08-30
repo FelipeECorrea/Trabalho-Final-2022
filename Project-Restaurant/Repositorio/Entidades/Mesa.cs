20e390f0bb2e602d344a075e2aa7c351207d8001
AlanDominguesFernandes28 <alan786k@gmail.com>
2022-08-29 21:16:29 -0300

    mesa criada

---
+++ Project-Restaurant/Repositorio/Entidades/Mesa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio.Entidades
+{
+    public class Mesa
+    {
+        public int id { get; set; }
+        public int NumeroMesa { get; set; }
+    }
+}
