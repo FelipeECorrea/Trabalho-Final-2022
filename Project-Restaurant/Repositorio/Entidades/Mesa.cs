f073e497e6178c895fcb399d7f55913b8c3332f5
AlanDominguesFernandes28 <alan786k@gmail.com>
2022-08-31 18:27:17 -0300

    Mapeamento sendo feito

--- Project-Restaurant/Repositorio/Entidades/Mesa.cs
+++ Project-Restaurant/Repositorio/Entidades/Mesa.cs
@@ -8,6 +8,6 @@ namespace Repositorio.Entidades
 {
     public class Mesa : EntidadeBase
     {
-        public int NumeroMesa { get; set; }
+        public int numeroMesa { get; set; }
     }
 }
--- Project-Restaurant/Repositorio/Mapeamentos/MesaMapeamento.cs
+++ Project-Restaurant/Repositorio/Mapeamentos/MesaMapeamento.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Repositorio.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,58 @@ using System.Threading.Tasks;
 
 namespace Repositorio.Mapeamentos
 {
-    public  class MesaMapeamento
+    public  class MesaMapeamento : IEntityTypeConfiguration<Mesa>
     {
+        public void Configure(EntityTypeBuilder<Mesa> builder)
+        {
+            builder.ToTable("mesas");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.NumeroMesa)
+           .HasColumnType("INTEGER")
+           .IsRequired()
+           .HasColumnName("numeroMesa"); // NOT NULL
+
+            builder.HasData(
+                new Mesa
+                {
+                    Id = 1,
+                    numeroMesa = 1
+                    
+                },
+                new Mesa
+                {
+                    Id = 2,
+                    numeroMesa = 2
+                    
+                },
+                new Mesa
+                {
+                    Id = 3,
+                    numeroMesa = 3
+
+                }, 
+                new Mesa
+                {
+                    Id = 4,
+                    numeroMesa = 4
+
+                },
+                new Mesa
+                {
+                    Id = 5,
+                    numeroMesa = 5
+
+                },
+
+                new Mesa
+                {
+                    Id = 2,
+                    numeroMesa = 6
+                }
+               );
+        }
 
     }
 }
