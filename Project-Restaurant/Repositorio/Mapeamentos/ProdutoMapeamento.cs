39455b7c5b327625ad2c6c7e860ae47866d7c849
marina.lima <marinalimasc@gmail.com>
2022-08-31 21:49:08 -0300

    Finalização do ProdutoMapeamento

--- Project-Restaurant/Repositorio/Mapeamentos/ProdutoMapeamento.cs
+++ Project-Restaurant/Repositorio/Mapeamentos/ProdutoMapeamento.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Repositorio.Entidades;
 
 namespace Repositorio.Mapeamentos
 {
     internal class ProdutoMapeamento : IEntityTypeConfiguration<Produto>
     {
-    public void Configure(EntityTypeBuilder<Produto> builder)
+        public void Configure(EntityTypeBuilder<Produto> builder)
         {
             builder.ToTable("produtos");
 
@@ -22,7 +23,45 @@ namespace Repositorio.Mapeamentos
               .IsRequired()
               .HasColumnName("nome");
 
+            builder.Property(x => x.Categoria)
+               .HasColumnType("VARCHAR")
+               .HasMaxLength(1000)
+               .IsRequired()
+               .HasColumnName("categoria");
 
+            builder.Property(x => x.Descricao)
+             .HasColumnType("VARCHAR")
+             .HasMaxLength(200)
+             .IsRequired()
+             .HasColumnName("descricao");
+
+
+            builder.Property(x => x.ProdutoCaminho)
+               .HasColumnType("VARCHAR")
+               .HasMaxLength(1000)
+               .IsRequired()
+               .HasColumnName("produto_caminho");
+
+            builder.HasData(
+               new Produto
+               {
+                   Id = 1,
+                   Valor = 20,
+                   Nome = "Yakissoba",
+                   Categoria = "Massas",
+                   Descricao = "Yakissoba de frango e legumes",
+                   ProdutoCaminho = "favicon.ico"
+               },
+               new Produto
+               {
+                   Id = 2,
+                   Valor = 6,
+                   Nome = "Coca-cola 600ml",
+                   Categoria = "Bebidas",
+                   Descricao = "Coca-cola 600ml",
+                   ProdutoCaminho = "favicon.ico"
+
+               });
         }
     }
 }
