81d5c307d5ac00bb5b36f38c4ad888ec5fc5b022
cristyan.alexandrino <cristyanalexandrino.od@gmail.com>
2022-09-05 19:26:03 -0300

    Entidade Cliente com IList Produto Pedido

--- Project-Restaurant/Repositorio/Entidades/Cliente.cs
+++ Project-Restaurant/Repositorio/Entidades/Cliente.cs
@@ -1,12 +1,14 @@
 ﻿namespace Repositorio.Entidades
 {
     public class Cliente : EntidadeBase
-    { 
+    {
         public string Nome { get; set; }
         public string Telefone { get; set; }
         public string Cpf { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
         public List<Pedido> Pedidos { get; set; }
+        public IList<ProdutoPedido> ProdutosPedidos { get; set; }
+
     }
 }
