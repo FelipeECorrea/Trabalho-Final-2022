b90eac5f024bd495a9dd0018d6b84f07e5003d2a
cristyan.alexandrino <cristyanalexandrino.od@gmail.com>
2022-09-05 20:12:49 -0300

    Adiciona Cliente em Restaurante Contexto

--- Project-Restaurant/Repositorio/BancoDados/RestauranteContexto.cs
+++ Project-Restaurant/Repositorio/BancoDados/RestauranteContexto.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Repositorio.Entidades;
+using Repositorio.Mapeamentos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,7 @@ namespace Repositorio.BancoDados
 {
     public class RestauranteContexto : DbContext
     {
-
+        public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Produto> Produtos { get; set; }
 
         public RestauranteContexto(
@@ -21,7 +22,8 @@ namespace Repositorio.BancoDados
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            modelBuilder.ApplyConfiguration(new ClienteMapeamento());
         }
+
     }
 }
