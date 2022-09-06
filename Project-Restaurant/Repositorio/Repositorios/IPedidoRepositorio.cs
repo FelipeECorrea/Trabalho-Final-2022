c13a310c896d311202333648c33b76d3a6b78757
Felipe Correa <pessoal.fec@gmail.com>
2022-09-06 09:46:50 -0300

    Incrementando Repositorio Pedido ao projeto

---
+++ Project-Restaurant/Repositorio/Repositorios/IPedidoRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Repositorio.Entidades;
+
+namespace Repositorio.Repositorios
+{
+    public interface IPedidoRepositorio
+    {
+        Pedido Cadastrar(Pedido pedido);
+        Pedido? Apagar(int id);
+        Pedido? ObterPodId(int pedidoId);
+        void Editar(Pedido pedido);
+        IList<Pedido> ObterTodos(); //TODO vERIFICAR COM QUERIA COMO SERA LISTADO OS PEDIDOS
+    }
+}
