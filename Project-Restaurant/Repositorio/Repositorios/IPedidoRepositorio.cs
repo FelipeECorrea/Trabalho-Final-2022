33fddf361c8913134c4a23702160faca13a2b8d0
Felipe Evanilson Correa <pessoal.fec@gmail.com>
2022-09-05 22:10:10 -0300

    Criando RepositorioPedido

---
+++ Project-Restaurant/Repositorio/Repositorios/IPedidoRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Repositorio.Entidades;
+
+namespace Repositorio.Repositorios
+{
+    public interface IPedidoRepositorio
+    {
+        Pedido Cadastrar(Pedido pedido);
+        Pedido? Apagar(int id);
+        void Editar(Pedido pedido);
+        Pedido? ObterPodId(int PedidoId);
+        IList<Pedido> ObterTodos();
+    }
+}
---
+++ Project-Restaurant/Repositorio/Repositorios/PedidoRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Repositorio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio.Repositorios
+{
+    public class PedidoRepositorio : IPedidoRepositorio
+    {
+    }
+}
