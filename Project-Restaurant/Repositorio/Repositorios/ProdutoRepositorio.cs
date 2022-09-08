d85eb91db9035b614eb96623a7ce0bd66c6ed520
Marina De Lima <marinalimasc@gmail.com>
2022-09-08 18:30:16 -0300

    Criado o repositório de Produtos

--- Project-Restaurant/Repositorio/Repositorios/ProdutoRepositorio.cs
+++ Project-Restaurant/Repositorio/Repositorios/ProdutoRepositorio.cs
@@ -1,4 +1,6 @@
-﻿using Repositorio.Entidades;
+﻿using Microsoft.EntityFrameworkCore;
+using Repositorio.BancoDados;
+using Repositorio.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,29 +11,50 @@ namespace Repositorio.Repositorios
 {
     public class ProdutoRepositorio : IProdutoRepositorio
     {
+
+        private readonly RestauranteContexto _contexto;
+
+
         public bool Apagar(int id)
         {
-            throw new NotImplementedException();
+
+            var produto = _contexto.Produtos
+                .FirstOrDefault(x => x.Id == id);
+
+            if (produto == null)
+                return false;
+
+            _contexto.Produtos.Remove(produto);
+            _contexto.SaveChanges();
+
+            return true;
         }
 
         public Produto Cadastrar(Produto produto)
         {
-            throw new NotImplementedException();
+            _contexto.Produtos.Add(produto);
+            _contexto.SaveChanges();
+
+            return produto;
         }
 
         public void Editar(Produto produto)
         {
-            throw new NotImplementedException();
+
+            _contexto.Produtos.Update(produto);
+            _contexto.SaveChanges();
         }
 
-        public Produto? ObterPodId(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public Produto? ObterPodId(int id) =>
+               _contexto.Produtos
+            .Include(x => x.ProdutosPedidos)
+            .FirstOrDefault(x => x.Id == id);
 
-        public IList<Produto> ObterTodos()
-        {
-            throw new NotImplementedException();
+
+
+        public IList<Produto> ObterTodos() =>
+        
+            _contexto.Produtos.ToList();
         }
     }
-}
+
