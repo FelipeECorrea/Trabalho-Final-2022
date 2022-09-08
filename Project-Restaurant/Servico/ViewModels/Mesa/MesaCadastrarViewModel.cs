a89f53e35df81aea5312168c737441e7a72d5751
AlanDominguesFernandes28 <alan786k@gmail.com>
2022-09-08 19:45:21 -0300

    Mesa ViewModel em elaboração

---
+++ Project-Restaurant/Servico/ViewModels/Mesa/MesaCadastrarViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servico.ViewModels.Mesa
+{
+    public class MesaCadastrarViewModel
+    {
+        public byte Status { get; set; }
+
+        [Display(Name = nameof(NumeroMesa))]
+        [Required(ErrorMessage = "{0} deve ser preenchido")]
+        [MinLength(1, ErrorMessage = "{0} deve conter no mínimo {1} caracteres")]
+        [MaxLength(6, ErrorMessage = "{0} deve conter no máximo {1} caracteres")]
+        public int NumeroMesa { get; set; }
+    }
+}
