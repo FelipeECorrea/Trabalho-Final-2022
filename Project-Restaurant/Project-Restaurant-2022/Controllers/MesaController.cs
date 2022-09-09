3335a8ba713a9640d3250bc92263f8ca2f2e9ca6
AlanDominguesFernandes28 <alan786k@gmail.com>
2022-09-09 17:46:26 -0300

    controler criado

--- Project-Restaurant/Project-Restaurant-2022/Controllers/MesaController.cs
+++ Project-Restaurant/Project-Restaurant-2022/Controllers/MesaController.cs
@@ -1,6 +1,27 @@
-﻿namespace Project_Restaurant_2022.Controllers
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servico.Servicos;
+using Servico.ViewModels.Produto;
+
+namespace Project_Restaurant_2022.Controllers
 {
-    public class MesaController
+    [Route("mesa")]
+    public class MesaController : Controller
     {
-    }
-}
+        private readonly IMesaService _mesaService;
+
+        public MesaController(IMesaService mesaService)
+        {
+            _mesaService = mesaService;
+        }
+
+       
+        public ActionResult Index()
+        {
+            var mesas = _mesaService.ObterTodos();
+            return View(mesas);
+        }
+
+
+    }   
+}   
