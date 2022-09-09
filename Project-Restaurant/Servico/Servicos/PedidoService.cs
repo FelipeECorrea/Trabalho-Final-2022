d15f7bfa94a15160f39dc1d29cab80452c3ae206
Felipe Evanilson Correa <pessoal.fec@gmail.com>
2022-09-08 21:44:37 -0300

    ViewModel setor Pedido

--- Project-Restaurant/Servico/Servicos/PedidoService.cs
+++ Project-Restaurant/Servico/Servicos/PedidoService.cs
@@ -1,12 +1,51 @@
-﻿using Repositorio.Mapeamentos;
+﻿using Repositorio.Entidades;
+using Repositorio.Mapeamentos;
 using Repositorio.Repositorios;
+using Servico.ViewModels;
+using Servico.ViewModels.Pedido;
 
 namespace Servico.Servicos
 {
-    public class PedidoService // : IPedidoService
+    public class PedidoService : IPedidoService
     {
         private readonly IPedidoRepositorio _pedidoRepositorio;
-        private readonly PedidoMapeamento _mapeamentoPedidoEntidade;
-        //private readonly IPedidoViewModel _MapeamentoPedidoViewModel;
+
+        public void Apagar(int id)
+        {
+            _pedidoRepositorio.Apagar(id);
+        }
+
+        public void Cadastrar(PedidoCadastrarViewModel pedidoCadastrarViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pedido ObterPorId(int pedidoId)
+        {
+            var BuscarPedidoPorId = _pedidoRepositorio.ObterPorId(pedidoId);
+
+            return BuscarPedidoPorId;
+        }
+
+        public IList<Pedido> ObterTodos()
+        {
+            var pedidosDoBanco = _pedidoRepositorio.ObterTodos();
+
+            return pedidosDoBanco;
+        }
+
+        public IList<SelectViewModel> ObterTodosPedidos()
+        {
+            var pedidos = _pedidoRepositorio.ObterTodos();
+
+            var selectViewModel = pedidos
+                .Select(x => new SelectViewModel
+                {
+                    Id = x.Id,
+                })
+                .ToList();
+
+            return selectViewModel;
+        }
     }
 }
