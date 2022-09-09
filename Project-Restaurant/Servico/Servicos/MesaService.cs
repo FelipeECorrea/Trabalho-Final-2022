5c46094dc21eb5feaba2733295408c0eb6e338bf
AlanDominguesFernandes28 <alan786k@gmail.com>
2022-09-09 17:42:48 -0300

    mesa service

--- Project-Restaurant/Servico/Servicos/MesaService.cs
+++ Project-Restaurant/Servico/Servicos/MesaService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Repositorio.Entidades;
+using Repositorio.Repositorios;
+using Servico.MapeamentoViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,25 @@ using System.Threading.Tasks;
 
 namespace Servico.Servicos
 {
-    public class MesaService
+    public class MesaService : IMesaService
     {
+        private readonly IMesaRepositorio _mesaRepositorio;
+        private readonly IMesaViewModelMapeamentoViewModels _mapeamentoViewModel;
+
+        public MesaService(
+            IMesaRepositorio mesaRepositorio,
+            IMesaViewModelMapeamentoViewModels mapeamentoViewModel)
+        {
+            _mesaRepositorio = mesaRepositorio;
+            _mapeamentoViewModel = mapeamentoViewModel;
+        }
+        public Mesa? ObterPorId(int id) =>
+            _mesaRepositorio.ObterPorId(id);
+
+        public IList<Mesa> ObterTodos() =>
+            _mesaRepositorio.ObterTodos();
 
     }
+
+
 }
