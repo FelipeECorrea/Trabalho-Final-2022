6f4767c92460a9eb472d7c439a772ae29de927d3
Felipe Evanilson Correa <pessoal.fec@gmail.com>
2022-09-14 20:21:40 -0300

    Implementando os selects na tela de cadastrar pedido

---
+++ Project-Restaurant/Project-Restaurant-2022/wwwroot/js/Pedido/pedidos-select2.js
@@ -0,0 +1,15 @@
+﻿$('#cadastroCliente').select2({
+    ajax: {
+        url: '/Cliente/obterTodosSelect2',
+        dataType: 'json',
+        processResults: (data) => ({ results: data })
+    }
+});
+
+$('#cadastroMesa').select2({
+    ajax: {
+        url: '/Mesa/obterTodosSelect2',
+        dataType: 'json',
+        processResults: (data) => ({ results: data })
+    }
+});
