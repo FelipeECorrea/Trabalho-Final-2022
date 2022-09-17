a614ff4a0a030f341f65eeef4ccf9f892d6b4486
joaomarti755 <joaomarti755@gmail.com>
2022-09-16 22:05:51 -0300

    Criação do ProdutoPedidoViewModelMapeamentoViewModel

---
+++ Project-Restaurant/Servico/MapeamentoViewModels/ProdutoPedidoViewModelMapeamentoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Repositorio.Entidades;
+using Servico.ViewModels.ProdutoPedido;
+
+namespace Servico.MapeamentoViewModels
+{
+    internal class ProdutoPedidoViewModelMapeamentoViewModel : IProdutoPedidoViewModelMapeamentoViewModel
+    {
+        public ProdutoPedidoViewModel ConstruirCom(ProdutoPedido produtoPedido) =>
+            new ProdutoPedidoCadastrarViewModel
+            {
+                 PedidoId = produtoPedido.PedidoId,
+                 ProdutoId = produtoPedido.ProdutoId,
+                 Quantidade = produtoPedido.Quantidade,
+                 Valor = produtoPedido.Valor
+            };
+    }
+}
