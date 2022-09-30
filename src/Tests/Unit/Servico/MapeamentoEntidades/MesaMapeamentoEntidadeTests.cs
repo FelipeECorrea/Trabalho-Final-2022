da835cbd0b72e66f177b3f843cbe40730cdcd5b6
AlanDominguesFernandes28 <alan786k@gmail.com>
2022-09-29 22:02:44 -0300

    Criação de testes unitarios

---
+++ src/Tests/Unit/Servico/MapeamentoEntidades/MesaMapeamentoEntidadeTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Servico.MapeamentoEntidades;
+using Servico.ViewModels.Mesa;
+using Servico.ViewModels.Pedido;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testes.Unit.Servico.MapeamentoEntidades
+{
+    internal class MesaMapeamentoEntidadeTests
+    {
+        private readonly IMesaMapeamentoEntidade _mesaMapeamentoEntidade;
+
+        public MesaMapeamentoEntidadeTests()
+        {
+            _mesaMapeamentoEntidade = new MesaMapeamentoEntidade();
+        }
+        public void Test_ConstruirCom_()
+        {
+            // Arrange
+            var viewModel = new MesaCadastrarViewModel
+            {
+                Status = 1,
+                NumeroMesa = 2
+            };
+
+            // Act
+            _mesaMapeamentoEntidade.ConstruirCom(viewModel);
+
+            // Assert
+            viewModel.Status.Should().Be(viewModel.Status);
+            viewModel.NumeroMesa.Should().Be(viewModel.NumeroMesa);
+            
+
+        }
+    }
+}
