ef25c7a221b7bcc8108fc9b5fd2aa5f2bd755fc2
marina.lima <marinalimasc@gmail.com>
2022-10-07 19:18:29 -0300

    Commit inicial

---
+++ Project-Restaurant/Project-Restaurant-2022/Controllers/ProdutoController.cs
@@ -0,0 +1,7 @@
+﻿namespace Project_Restaurant_2022.Controllers
+{
+    public class ProdutoController
+    {
+
+    }
+}
---
+++ Project-Restaurant/Repositorio/Entidade/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio.Entidade
+{
+    // Entidades: são representações das tabelas para classe de objetos
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public string Nome { get; set; }
+
+        public string categoria { get; set; }
+
+        public string descricao { get; set; }
+
+    }
+}
