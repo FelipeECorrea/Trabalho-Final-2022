da5722bdbae307ed12049c0aae5a8f464c68621a
cristyan.alexandrino <cristyanalexandrino.od@gmail.com>
2022-10-10 18:21:09 -0300

    Merge Okay

--- Project-Restaurant/PublicoAplicacao/Controllers/ClienteLoginController.cs
+++ Project-Restaurant/PublicoAplicacao/Controllers/ClienteLoginController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Repositorio.Entidades;
 using Repositorio.Enums;
 using Servico.Servicos;
 using Servico.ViewModels.Cliente;
+using Servico.ViewModels.Login;
 using Servico.ViewModels.LoginCliente;
 
 namespace PublicoAplicacao.Controllers
@@ -88,6 +90,36 @@ namespace PublicoAplicacao.Controllers
                .OrderBy(x => x)
                .ToList();
         }
+
+        [HttpPost]
+        public IActionResult Entrar(LoginViewModel loginViewModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Cliente cliente = _clienteService.ObterPorEmail(loginViewModel.Email);
+
+                    if (cliente != null)
+                    {
+                        if (cliente.Senha == loginViewModel.Senha)
+                        {
+                            return RedirectToAction("Cardapio");
+                        }
+
+                        TempData["MensageErro"] = $"senha inválida. Por favor, tente novamente.";
+                    }
+
+                    TempData["MensageErro"] = $"Usuário e/ou senha inválido(s). Por favor, tente novamente.";
+                }
+                return View("Index");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensageErro"] = $"Ops, não conseguimos realizar seu login, tente novamente, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
-        
+
