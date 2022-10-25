e6ca4c7a43a211ce46dbefa90604dbf6e9950a8b
Cristyan Alexandrino <cristyanalexandrino.od@gmail.com>
2022-10-25 12:31:39 -0300

    Mascaras de Tel e CPF

---
+++ src/Repositorio/Migrations/20221025020331_MudandoMascaras.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositorio.Migrations
+{
+    public partial class MudandoMascaras : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "telefone",
+                table: "clientes",
+                type: "VARCHAR(14)",
+                maxLength: 14,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(11)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "cpf",
+                table: "clientes",
+                type: "VARCHAR(14)",
+                maxLength: 14,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(11)",
+                oldMaxLength: 11);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "telefone",
+                table: "clientes",
+                type: "VARCHAR(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(14)",
+                oldMaxLength: 14);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "cpf",
+                table: "clientes",
+                type: "VARCHAR(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(14)",
+                oldMaxLength: 14);
+        }
+    }
+}
